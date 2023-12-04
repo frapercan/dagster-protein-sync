73e6da2136cb123f7208c348322c96f298fa3990
frapercan <frapercan1@alum.us.es>
2023-12-04 13:50:24 +0100

    Logs fix

--- repo.py
+++ repo.py
@@ -5,11 +5,13 @@ from sqlalchemy.orm import sessionmaker
 from dagster import op, job, ScheduleDefinition
 
 from protein_data_handler.helpers.config.yaml import read_yaml_config
-from protein_data_handler.models.uniprot import Base
+from protein_data_handler.models.uniprot import Base, PDBReference
 from protein_data_handler.uniprot import cargar_codigos_acceso, extraer_entradas
 from protein_data_handler.pdb import download_entire_pdb
+from protein_data_handler.fasta import FastaHandler
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 
 def get_database_session():
     config = read_yaml_config("./config/config.yaml")
@@ -23,6 +25,7 @@ def get_database_session():
     Session = sessionmaker(bind=engine)
     return Session()
 
+
 @op
 def cargar_codigos_op():
     """Carga códigos de acceso de UniProt."""
@@ -30,11 +33,13 @@ def cargar_codigos_op():
     config = read_yaml_config("./config/config.yaml")
     cargar_codigos_acceso(criterio_busqueda=config['criterio_busqueda'], limite=config['limit'], session=session)
 
+
 @op
 def procesar_datos_uniprot_op(cargar_codigos):
     """Procesa datos de UniProt."""
     session = get_database_session()
     extraer_entradas(session=session)
+
 
 @op
 def descargar_pdb_op():
@@ -44,11 +49,35 @@ def descargar_pdb_op():
     file_format = config['file_format']
     download_entire_pdb(server, pdb, file_format)
 
-from dagster import job
+
+def initialize_fasta_handler(config):
+    session = get_database_session()
+    return FastaHandler(session, config['data_dir'], config['output_dir'])
+
+
+@op
+def descargar_mergear_fastas_op(procesar_datos_uniprot_op):
+    """
+    Operación para descargar archivos FASTA.
+    """
+    config = read_yaml_config("./config/config.yaml")
+    fasta_handler = initialize_fasta_handler(config)
+    logging.info("Descargando archivos FASTA...")
+    session = get_database_session()
+    query = session.query(PDBReference).filter(PDBReference.resolution < config.get("resolution_threshold", 2.5)).all()
+    pdb_ids = [pdb_ref.pdb_id for pdb_ref in query]
+    # Inicializa FastaHandler
+    fasta_downloader = FastaHandler(session, config['data_dir'], config['output_dir'])
+
+    # Descarga los archivos FASTA
+    fasta_downloader.download_fastas(pdb_ids, config['max_workers'])
+
+    fasta_downloader.merge_fastas(pdb_ids, config['merge_name'])
+
 
 @job
 def protein_data_pipeline():
-    procesar_datos_uniprot_op(cargar_codigos_op())
+    descargar_mergear_fastas_op(procesar_datos_uniprot_op(cargar_codigos_op()))
     descargar_pdb_op()
 
 
